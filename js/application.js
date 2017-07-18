d5625234181b2df6087752c992a567bab8925490
rodolfo <dolfogaspar@gmail.com>
2017-07-18 13:19:12 -0300

    Final da aula de javascript e primeira parte da aula de jquery

--- js/application.js
+++ js/application.js
@@ -1,7 +1,12 @@
 $(function(){   
-// window.onload = function(){
-    alert("Olá!");
-    console.log('lalalalala');
-    document.getElementById('fp').className='bluecolor';
+// // window.onload = function(){
+//     alert("Olá!");
+//     console.log('lalalalala');
+    
 // }
 })
+
+function changeToBlue(){
+    // document.getElementById('fp').className='bluecolor';
+    $('#fp').addclass('bluecolor')
+}
